6316b7d2ed756745f4ad35af65f84ae61cca1964
Shetia C. Butler Lamar <36701865+lamars18@users.noreply.github.com>
2018-07-09 19:53:57 -0400

    Update scriptSlider.js

--- javascript/scriptSlider.js
+++ javascript/scriptSlider.js
@@ -7,12 +7,15 @@ $(document).ready(function () {
   speed: 300,
   slidesToShow: 1,
   slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        autoplay: true,
         infinite: true,
         dots: true
       }
@@ -21,14 +24,16 @@ $(document).ready(function () {
       breakpoint: 600,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true
       }
     },
     {
       breakpoint: 480,
       settings: {
         slidesToShow: 1,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        autoplay: true
       }
     }
     // You can unslick at a given breakpoint now by adding:
