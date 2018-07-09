63400c5c4b2acabb6dedf359b97b3794d0bd9ab6
Shetia C. Butler Lamar <36701865+lamars18@users.noreply.github.com>
2018-07-09 19:16:30 -0400

    Update scriptSlider.js

--- javascript/scriptSlider.js
+++ javascript/scriptSlider.js
@@ -1,18 +1,18 @@
 
 $(document).ready(function () {
 
- $('.autoplay').slick({
+ $('.responsive').slick({
   dots: true,
   infinite: false,
   speed: 300,
-  slidesToShow: 4,
-  slidesToScroll: 4,
+  slidesToShow: 1,
+  slidesToScroll: 1,
   responsive: [
     {
       breakpoint: 1024,
       settings: {
-        slidesToShow: 3,
-        slidesToScroll: 3,
+        slidesToShow: 1,
+        slidesToScroll: 1,
         infinite: true,
         dots: true
       }
@@ -20,8 +20,8 @@ $(document).ready(function () {
     {
       breakpoint: 600,
       settings: {
-        slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToShow: 1,
+        slidesToScroll: 1
       }
     },
     {
